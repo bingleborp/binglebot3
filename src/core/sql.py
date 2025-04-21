8d958541c35d336cd7cb33cf78ac6e2b654c5693
Vladislav <alykashi@gmail.com>
2025-04-21 21:03:24 +0300

    Feat: use context manager

--- src/core/sql.py
+++ src/core/sql.py
@@ -2,54 +2,39 @@ from .database_connection import connect_to_db
 
 def do(query: str) -> str:
     try:
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        
-        cursor.execute(query)
+        with connect_to_db() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
 
-        if cursor.description:  
-            result = cursor.fetchall()
-            response = "Результат:\n" + "\n".join(str(row) for row in result)
-        else:  
-            conn.commit()
-            response = f"Успешно. Затронуто строк: {cursor.rowcount}"
-
+                if cursor.description:  
+                    result = cursor.fetchall()
+                    response = "Результат:\n" + "\n".join(str(row) for row in result)
+                else:  
+                    conn.commit()
+                    response = f"Успешно. Затронуто строк: {cursor.rowcount}"
+    
     except Exception as e:
         response = f"Ошибка: {str(e)}"
-        
-    finally:
-        if 'conn' in locals():  
-            cursor.close()
-            conn.close()
+    
     return response  
 
 def get_user_name_by_id(user_id: int) -> str:
     query = "SELECT name FROM users WHERE telegram_id = %s"
     try:
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        cursor.execute(query, (user_id,))           
-        response = cursor.fetchone()
+        with connect_to_db() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (user_id,))           
+                result = cursor.fetchone()
+                response = result[0] if result else None
     except Exception as e:
-        response = f"Ошибка!"
-    finally:
-        if 'conn' in locals():  
-            cursor.close()
-            conn.close()
+        response = f"Ошибка: {str(e)}"
+    
     return response
 
 def is_moder(user_id: int) -> bool:
-    query = "SELECT 1 FROM users WHERE telegram_id = %s AND is_moder = %s"
-    try:
-        conn = connect_to_db()
-        with conn.cursor() as cursor: 
-            cursor.execute(query, (user_id, True))
-            return cursor.fetchone() is not None  
-            
-    except Exception as e:
-        print(f"Ошибка при проверке модератора: {e}")
-        return False  
-        
-    finally:
-        if 'conn' in locals():
-            conn.close()  
+    with connect_to_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT EXISTS(SELECT 1 FROM users WHERE telegram_id = %s AND is_moder = %s)",
+                (user_id, True))
+            return cursor.fetchone()[0] 
