c0ea02a2302c56adc2018cdc6a3004f9c2361f0b
Vladislav <alykashi@gmail.com>
2025-04-21 18:16:20 +0300

    FIx: upgrade sql command

---
+++ src/core/sql.py
@@ -0,0 +1,25 @@
+from core.database_connection import connect_to_db
+
+def do(query: str) -> str:
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        
+        cursor.execute(query)
+
+        if cursor.description:  
+            result = cursor.fetchall()
+            response = "Результат:\n" + "\n".join(str(row) for row in result)
+        else:  
+            conn.commit()
+            response = f"Успешно. Затронуто строк: {cursor.rowcount}"
+
+    except Exception as e:
+        response = f"Ошибка: {str(e)}"
+        
+    finally:
+        if 'conn' in locals():  
+            cursor.close()
+            conn.close()
+
+    return response  
--- src/handlers/sql_handler.py
+++ src/handlers/sql_handler.py
@@ -1,6 +1,6 @@
 from ..core.bot_instance import bot
-from ..core.database_connection import connect_to_db
-from ..helpers.is_admin_helper import is_admin
+from ..core.sql import operation
+from ..core.is_admin_helper import is_admin
 
 def register_handlers():
     # Обработчик команды /sql
@@ -20,32 +20,7 @@ def register_handlers():
         if message.content_type != 'text':
             bot.send_message(message.chat.id, "Нужен текстовый запрос!")
             return
-
-        try:
-            conn = connect_to_db()
-            cursor = conn.cursor()
-            query = message.text
-            
-            # Выполняем запрос
-            cursor.execute(query)
-
-            if cursor.description:
-                # SELECT-запрос
-                result = cursor.fetchall()
-                response = "Результат:\n" + "\n".join(str(row) for row in result)
-            else:
-                # UPDATE/INSERT/DELETE
-                conn.commit()
-                response = f"Успешно. Затронуто строк: {cursor.rowcount}"
-
-        except Exception as e:
-            response = f"Ошибка: {str(e)}"
-            
-        finally:
-            # Закрываем соединение
-            if 'conn' in locals():
-                cursor.close()
-                conn.close()
+        response = operation(message.text)
 
         # Отправляем результат
         bot.send_message(message.chat.id, response)
