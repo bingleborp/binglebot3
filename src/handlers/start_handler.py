3b9b55b6f76feb90760892f49f912571d67cb958
Vladislav <alykashi@gmail.com>
2025-04-21 21:26:18 +0300

    Fix: bad import

--- src/handlers/start_handler.py
+++ src/handlers/start_handler.py
@@ -1,8 +1,8 @@
 from ..core.bot_instance import bot
-from ..helpers.users import register
+from ..helpers.users import register_user
 
 def register_handlers():
     @bot.message_handler(commands=["start"])
     def start(m, res=False):
-        register(m.chat.id, m.from_user.first_name)
+        register_user(m.chat.id, m.from_user.first_name)
         bot.send_message(m.chat.id, 'Я на связи! Напиши /help для списка команд')
